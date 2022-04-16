14558ed736198559cdac71e10a958b3a00dd059e
Thalia <xi1le7@gmail.com>
2022-04-16 15:38:13 -0400

    first.commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import './App.css';
+
+function App() {
+  return (
+    <>
+      React⚛️ + Vite⚡ + Replit🌀
+    </>
+  );
+}
+
+
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+)
